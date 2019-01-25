f19976af24ad86e37e31a5d8bc029c1c97d01e29
Yan Zhang <cyanogenoid@cyanogenoid.com>
2019-01-25 00:51:53 +0000

    Scale loss display more similarly to tfprocess

--- pytorch/training.py
+++ pytorch/training.py
@@ -153,8 +153,8 @@ class Session():
 
     def log_metrics(self, writer):
         writer.add_scalar('Losses/Policy', self.metric('policy_loss'), global_step=self.total_step)
-        writer.add_scalar('Losses/Value', self.metric('value_loss'), global_step=self.total_step)
-        writer.add_scalar('Losses/Weight', self.metric('reg_loss'), global_step=self.total_step)
+        writer.add_scalar('Losses/Value', self.metric('value_loss') / 4, global_step=self.total_step)
+        writer.add_scalar('Losses/Weight', self.metric('reg_loss') * 1e-4, global_step=self.total_step)
         writer.add_scalar('Losses/Total', self.metric('total_loss'), global_step=self.total_step)
         writer.add_scalar('Policy/Accuracy', self.metric('policy_accuracy'), global_step=self.total_step)
         self.reset_metrics()
