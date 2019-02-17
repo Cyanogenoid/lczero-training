b1ecfbf61646ef3b2bd3029afc8647f03ca39da1
Yan Zhang <cyanogenoid@cyanogenoid.com>
2019-02-17 23:31:12 +0000

    Faster ctz with lookup table

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -20,28 +20,22 @@ def named_variables(net):
     yield from net.named_buffers()
 
 
-def ctz(x):
-    if x == 0:
-        return None
-    n = 0
-    if x & 0xFFFFFFFF == 0:
-        n += 32
-        x >>= 32
-    if x & 0x0000FFFF == 0:
-        n += 16
-        x >>= 16
-    if x & 0x000000FF == 0:
-        n += 8
-        x >>= 8
-    if x & 0x0000000F == 0:
-        n += 4
-        x >>= 4
-    if x & 0x00000003 == 0:
-        n += 2
-        x >>= 2
-    if x & 0x00000001 == 0:
-        n += 1
-    return n
+Mod37BitPosition = (
+        32, 0, 1, 26, 2, 23, 27, 0, 3, 16, 24, 30, 28, 11, 0, 13, 4,
+  7, 17, 0, 25, 22, 31, 15, 29, 10, 12, 6, 0, 21, 14, 9, 5,
+  20, 8, 19, 18
+)
+
+def ctz(v):
+    right_half = v & 0x0000FFFF
+    if right_half != 0:
+        v = right_half
+        offset = 0
+    else:
+        v >>= 32  # only use left half
+        offset = 32
+    idx = Mod37BitPosition[(-v & v) % 37]
+    return idx + offset
 
 
 def bit_indices(x):
