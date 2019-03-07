2c3294cc92682437d5ee5966956ac7b8f1ebd722
Yan Zhang <cyanogenoid@cyanogenoid.com>
2019-03-07 22:42:14 +0000

    Fix unintended ReLU being applied in ResBlocks

--- pytorch/model.py
+++ pytorch/model.py
@@ -114,9 +114,12 @@ class ValueHead(nn.Sequential):
         ]))
 
 
-class ResidualBlock(nn.Sequential):
+class ResidualBlock(nn.Module):
     def __init__(self, channels, se_ratio):
-        super().__init__(OrderedDict([
+        super().__init__()
+        # ResidualBlock can't be an nn.Sequential, because it would try to apply self.relu2
+        # in the residual block even when not passed into the constructor
+        self.layers = nn.Sequential(OrderedDict([
             ('conv1', nn.Conv2d(channels, channels, 3, padding=1, bias=False)),
             ('bn1', nn.BatchNorm2d(channels)),
             ('relu', nn.ReLU(inplace=True)),
@@ -207,6 +210,9 @@ def extract_weights(m):
     elif isinstance(m, PolicyHead):
         yield from extract_weights(m.conv_block)
         yield from extract_weights(m.conv)
+
+    elif isinstance(m, ResidualBlock):
+        yield from m.layers
 
     elif isinstance(m, nn.Sequential):
         for layer in m:
