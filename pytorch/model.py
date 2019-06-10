411ab4a41d1acfb6ed0b1253394a84e4d1a3dbe0
Yan Zhang <cyanogenoid@cyanogenoid.com>
2019-06-10 05:08:28 +0100

    Self-scale after first conv in residual blocks

--- pytorch/model.py
+++ pytorch/model.py
@@ -120,13 +120,14 @@ class ResidualBlock(nn.Module):
         # ResidualBlock can't be an nn.Sequential, because it would try to apply self.relu2
         # in the residual block even when not passed into the constructor
         self.layers = nn.Sequential(OrderedDict([
-            ('conv1', nn.Conv2d(channels, channels, 3, padding=1, bias=False)),
-            ('bn1', nn.BatchNorm2d(channels)),
+            ('conv1', nn.Conv2d(channels, 2 * channels, 3, padding=1, bias=False)),
+            ('bn1', nn.BatchNorm2d(2 * channels)),
+            ('ss', SelfScale2()),
+
             ('relu', nn.ReLU(inplace=True)),
 
-            ('conv2', nn.Conv2d(channels, 2 * channels, 3, padding=1, bias=False)),
-            ('bn2', nn.BatchNorm2d(2 * channels)),
-            ('ss', SelfScale2()),
+            ('conv2', nn.Conv2d(channels, channels, 3, padding=1, bias=False)),
+            ('bn2', nn.BatchNorm2d(channels)),
 
             ('se', SqueezeExcitation(channels, se_ratio)),
         ]))
